de7a43ce1e836545a1e9c76bcd99411b2eaaac1b
Matvey <tod13yung@ya.ru>
2021-04-16 18:53:41 +0400

    upd to make it compile

--- ml/model.py
+++ ml/model.py
@@ -8,10 +8,14 @@ path = "forest.model"
 def learn_and_save(csv_path):
     forest = RandomForestClassifier()
     for _ in range(n_epoch):
-        forest.fit([10.,100], 0.)
+        forest.fit(
+            [[10.,100],[100.,1.]], 
+            [0., 1.])
 
     joblib.dump(forest, path)
     return
 
 def load() -> RandomForestClassifier:
     return joblib.load()
+
+learn_and_save("fjfjsdifjsdifj.csv")
