77acfaeaeb10fd79754d1e36d60de2ada62a979c
Matvey <tod13yung@ya.ru>
2021-04-17 15:48:10 +0400

    upd to read csv

--- ml/model.py
+++ ml/model.py
@@ -1,21 +1,32 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
 import joblib
 
 n_epoch = 100
-path = "forest.model"
+path = "tree.model"
+
+''' .csv file example:
+
+Companies,Employers,Vacancies,Graduaters,GRP,Class
+0.0217,0.0167,0.1199,0.0574,0.0213,0
+'''
 
 
 def learn_and_save(csv_path):
-    forest = RandomForestClassifier()
-    for _ in range(n_epoch):
-        forest.fit(
-            [[10.,100],[100.,1.]], 
-            [0., 1.])
+    tree = DecisionTreeClassifier()
+    dataset = pd.read_csv(csv_path, sep=",", names=["Companies","Employers","Vacancies","Graduaters","GRP","Class"], header=0)
 
-    joblib.dump(forest, path)
+    data = list()
+    target = list()
+    for (com, empl, vac, grad, grp, cl) in zip(dataset["Companies"], dataset["Employers"], dataset["Vacancies"], dataset["Graduaters"], dataset["GRP"], dataset["Class"]):
+        data.append([com,empl,vac,grad,grp])
+        target.append(cl)
+
+    for _ in range(n_epoch):
+        tree.fit( data, target )
+
+    joblib.dump(tree, path)
     return
 
-def load() -> RandomForestClassifier:
-    return joblib.load()
-
-learn_and_save("fjfjsdifjsdifj.csv")
+def load() -> DecisionTreeClassifier:
+    return joblib.load(path)
